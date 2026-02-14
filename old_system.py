55c82d27c1440c704e745c9755821e29d153ddef
mf13-mra <mf13@kent.ac.uk>
2026-02-14 00:13:17 +0000

    7

--- old_system.py
+++ old_system.py
@@ -57,9 +57,9 @@ def run_system_monolith():
             count = 0
             
             for rank in r:
-                if rank == "Captain" or "Commander": 
+                if rank == "Captain" or rank == "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            print("High ranking officers: " + str(count)) 
             
         elif opt == "5":
             print("Shutting down.")
