80e6f8a9fb920bb812da689fb3e7b18e4378bd7d
mf13-mra <mf13@kent.ac.uk>
2026-02-15 17:28:33 +0000

    b

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,5 +1,9 @@
 def main():
     names, ranks, divs, ids = init_database()
+
+    choice = display_menu()
+
+
 def init_database():
     names = ["Spock", "James Kirk", "Jean-Luc Picard", "Seven of Nine", "Worf"]
     ranks = ["Commander", "Lieutenant", "Captain", "Cadet", "Ensign"]
@@ -8,9 +12,19 @@ def init_database():
 
     return names, ranks, divs, ids
 
-def display_menu():
-    currentuser = input("User please enter full name:")
+def display_menu() -> str:
+    currentUser = input("User please enter full name:\n")
 
+    while True: 
+        try:
+            print("Please make a selection" + " " + currentUser)
+            print("1) Show database.\n2) Add member.\n3) Remove member.\n4) Update rank.\n5) Search crew.\n6) Filter by division.\n7) Calculate payroll.\n8) Count officers.\n9) Quit.")
+            choice = int ( input("Please make a selection:\n"))
+            break
+        except:
+            print("Input invalid\n")
+
+    return choice
 
     
 
